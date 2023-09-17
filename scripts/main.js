d25885307dc73d15fdfdb9525dba800f0a7e658e
alibaba0010 <zakariyahali100@gmail.com>
2023-09-17 20:01:42 +0100

    Adding portfolio

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+(function() {
+  "use strict";
+
+  window.addEventListener('load', () => {
+    on_page_load()
+  });
+
+  /**
+   * Function gets called when page is loaded.
+   */
+  function on_page_load() {
+    // Initialize On-scroll Animations
+    AOS.init({
+      anchorPlacement: 'top-left',
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      disable: 'mobile'
+    });
+  }
+
+  /**
+   * Navbar effects and scrolltop buttons upon scrolling
+   */
+  const navbar = document.getElementById('header-nav')
+  var body = document.getElementsByTagName("body")[0]
+  const scrollTop = document.getElementById('scrolltop')
+  window.onscroll = () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add('fixed-top', 'shadow-sm')
+      body.style.paddingTop = navbar.offsetHeight + "px"
+      scrollTop.style.visibility = "visible";
+      scrollTop.style.opacity = 1;
+    } else {
+      navbar.classList.remove('fixed-top', 'shadow-sm')
+      body.style.paddingTop = "0px"
+      scrollTop.style.visibility = "hidden";
+      scrollTop.style.opacity = 0;
+    }
+  };
+
+})();
